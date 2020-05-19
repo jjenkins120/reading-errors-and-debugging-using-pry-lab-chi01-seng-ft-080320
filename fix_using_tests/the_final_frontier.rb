ac253e0c8e463460523505453c3d1b2228760ed2
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-19 15:56:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,10 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def generate_star_date
+def star_date
   (rand(100000) + 400000) / 10.0
 end
 
-date = generate_star_date
-
 def state_log(star_date)
-  star_date == 56790
   puts "Captain's Log, star date #{star_date}."
 end
 
