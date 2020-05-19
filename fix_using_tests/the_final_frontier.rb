e8ab627f3129c098b53b810ddda3816403b2e7b1
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-19 15:54:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,10 +7,9 @@ end
 date = generate_star_date
 
 def state_log(star_date)
+  star_date == 56790
   puts "Captain's Log, star date #{star_date}."
 end
-
-state_log(56790)
 
 def engage
   puts state_log(star_date)
